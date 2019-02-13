cc3672e401312edfa5873a1fdd4c3e0c8936521e
mlischke <miriam.lischke@iav.de>
2019-02-13 11:25:12 +0100

    add flight mockData

---
+++ src/mockData/flightData.js
@@ -0,0 +1,105 @@
+/* eslint-disable max-len */
+const data = [
+    {
+      id: 1,
+      title: 'Brüssels Airlines',
+      logo: 'http://web.brusselsairlines.com/facebook/image.gif',
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHHnc28Ifj7f1FcHAKlGC_GkBy79hH26hRo7X6kaO33C6OOb7W',
+      times: {
+        departe: {
+            start: '08:50 ',
+            end: '17:45'
+        },
+        arrive: {
+            start: '19:10',
+            end: '07:50'
+        }
+      },
+      price: {
+        single: '680.00 €',
+        Total: '3400.00 €'
+      }
+    },
+    {
+      id: 2,
+      title: 'Brüssels Airlines',
+      logo: 'http://web.brusselsairlines.com/facebook/image.gif',
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHHnc28Ifj7f1FcHAKlGC_GkBy79hH26hRo7X6kaO33C6OOb7W',
+      times: {
+        departe: {
+            start: '08:50 ',
+            end: '17:45'
+        },
+        arrive: {
+            start: '19:10',
+            end: '07:50'
+        }
+      },
+      price: {
+        single: '680.00 €',
+        Total: '3400.00 €'
+      }
+    },
+    {
+      id: 3,
+      title: 'Brüssels Airlines',
+      logo: 'http://web.brusselsairlines.com/facebook/image.gif',
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHHnc28Ifj7f1FcHAKlGC_GkBy79hH26hRo7X6kaO33C6OOb7W',
+      times: {
+        departe: {
+            start: '08:50 ',
+            end: '17:45'
+        },
+        arrive: {
+            start: '19:10',
+            end: '07:50'
+        }
+      },
+      price: {
+        single: '680.00 €',
+        Total: '3400.00 €'
+      }
+    },
+    {
+      id: 4,
+      title: 'Brüssels Airlines',
+      logo: 'http://web.brusselsairlines.com/facebook/image.gif',
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHHnc28Ifj7f1FcHAKlGC_GkBy79hH26hRo7X6kaO33C6OOb7W',
+      times: {
+        departe: {
+            start: '08:50 ',
+            end: '17:45'
+        },
+        arrive: {
+            start: '19:10',
+            end: '07:50'
+        }
+      },
+      price: {
+        single: '680.00 €',
+        Total: '3400.00 €'
+      }
+    },
+    {
+      id: 5,
+      title: 'Brüssels Airlines',
+      logo: 'http://web.brusselsairlines.com/facebook/image.gif',
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHHnc28Ifj7f1FcHAKlGC_GkBy79hH26hRo7X6kaO33C6OOb7W',
+      times: {
+        departe: {
+            start: '08:50 ',
+            end: '17:45'
+        },
+        arrive: {
+            start: '19:10',
+            end: '07:50'
+        }
+      },
+      price: {
+        single: '680.00 €',
+        Total: '3400.00 €'
+      }
+    },
+  ];
+  
+  export { data };
